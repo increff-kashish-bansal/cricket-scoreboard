14818a4667fffd64360d629f37254f155837054e
virat <kashish.bansal@increff.com>
2025-06-23 21:05:25 +0530

    Enhance CSV data with realistic blockers and developer names (unni, virat, shubham, ram, yukt)

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,29 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.')
+          const ext = info[info.length - 1]
+          if (/\.(css)$/.test(assetInfo.name)) {
+            return `assets/[name]-[hash].css`
+          }
+          if (/\.(png|jpe?g|gif|svg|ico|webp)$/.test(assetInfo.name)) {
+            return `assets/[name]-[hash].[ext]`
+          }
+          return `assets/[name]-[hash].[ext]`
+        }
+      }
+    }
+  },
+  server: {
+    headers: {
+      'X-Content-Type-Options': 'nosniff'
+    }
+  }
 })
